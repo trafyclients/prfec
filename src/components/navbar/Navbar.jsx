dd82c23a584023ba679b50f92a65cb12c85ffbce
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-09-21 10:19:07 +0530

    h

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,63 @@
+// 'use client';
+// import React, { useState, useEffect, useRef } from 'react';
+// import '@styles/navbar/Navbar.css';
+// import Link from 'next/link';
+// import Image from 'next/image';
+// import Logo from '@public/Images/navbar/prfec-logo.png';
+// import Hamburger from '@public/Images/navbar/hamburger.png';
+// import Close from '@public/Images/navbar/close.png';
+
+// export const Navbar = () => {
+//   const [menuOpen, setMenuOpen] = useState(false);
+//   const menuRef = useRef(null);
+
+//   const handleMenuClick = () => {
+//     setMenuOpen(!menuOpen);
+//   };
+
+//   useEffect(() => {
+//     const handleClickOutside = (event) => {
+//       if (menuRef.current && !menuRef.current.contains(event.target)) {
+//         setMenuOpen(false);
+//       }
+//     };
+
+//     document.addEventListener('mousedown', handleClickOutside);
+//     return () => {
+//       document.removeEventListener('mousedown', handleClickOutside);
+//     };
+//   }, []);
+
+//   return (
+//     <div className='navbar'>
+//       <div className='navbar-container'>
+//         <div className='navbar-logo'>
+//           <Link href="/">
+//             <Image className='prfec-logo' src={Logo} alt="Logo" />
+//           </Link>
+//         </div>
+
+//         <div className='navbar-contents'>
+//           <Link href="/login" className='navbar-login'>Log in</Link>
+//           <Link href="/signup" className='navbar-signup'>Sign up</Link>
+//         </div>
+
+//         <div className='navbar-menu-icons'>
+//           {!menuOpen && <Image src={Hamburger} alt="Menu" onClick={handleMenuClick} />}
+//           {menuOpen && <Image src={Close} alt="Close" onClick={handleMenuClick} />}
+//         </div>
+
+//         {menuOpen && (
+//           <div ref={menuRef} className='navbar-menu'>
+//             <Link href="/login" className='navbar-login'>Log in</Link>
+//             <Link href="/signup" className='navbar-signup'>Sign up</Link>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
 'use client';
 import React, { useState, useEffect, useRef } from 'react';
 import '@styles/navbar/Navbar.css';
@@ -13,6 +73,10 @@ export const Navbar = () => {
 
   const handleMenuClick = () => {
     setMenuOpen(!menuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setMenuOpen(false);
   };
 
   useEffect(() => {
@@ -32,14 +96,14 @@ export const Navbar = () => {
     <div className='navbar'>
       <div className='navbar-container'>
         <div className='navbar-logo'>
-          <Link href="/">
+          <Link href="/" onClick={handleLinkClick}>
             <Image className='prfec-logo' src={Logo} alt="Logo" />
           </Link>
         </div>
 
         <div className='navbar-contents'>
-          <Link href="/login" className='navbar-login'>Log in</Link>
-          <Link href="/signup" className='navbar-signup'>Sign up</Link>
+          <Link href="/login" className='navbar-login' onClick={handleLinkClick}>Log in</Link>
+          <Link href="/signup" className='navbar-signup' onClick={handleLinkClick}>Sign up</Link>
         </div>
 
         <div className='navbar-menu-icons'>
@@ -49,8 +113,8 @@ export const Navbar = () => {
 
         {menuOpen && (
           <div ref={menuRef} className='navbar-menu'>
-            <Link href="/login" className='navbar-login'>Log in</Link>
-            <Link href="/signup" className='navbar-signup'>Sign up</Link>
+            <Link href="/login" className='navbar-login' onClick={handleLinkClick}>Log in</Link>
+            <Link href="/signup" className='navbar-signup' onClick={handleLinkClick}>Sign up</Link>
           </div>
         )}
       </div>
